99123e0ccd892e09a3650237b19ff09b3bb355b5
Danny Chang <dannychachang@gmail.com>
2018-01-17 16:33:22 -0500

    Add edit, delete for comments

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,5 +13,29 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+
+    @review.comment = params[:review][:comment]
+
+    if @review.save
+      flash[:notice] = "Successfully edited review!"
+      redirect_to product_url(@review.product_id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    flash[:notice] = "Successfully deleted comment!"
+    redirect_to product_path(params[:product_id])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'products#index'
 
   resources :products do
-    resources :reviews, only: :create
+    resources :reviews, except: [:index, :new, :show]
   end
-  
+
 end

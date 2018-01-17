c9ab31defce25ae22db9cd75a1647d91523f0344
Danny Chang <dannychachang@gmail.com>
2018-01-17 12:18:50 -0500

    Add destroy controller, edit and delete links on the show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,10 @@ class ProductsController < ApplicationController
   def edit
   end
 
-  def delete
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to(products_path)
   end
 
 end

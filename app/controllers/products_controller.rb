7b7b19ede7ed3be41baef57015158c22552b1f80
Danny Chang <dannychachang@gmail.com>
2018-01-17 14:37:58 -0500

    Convert price from cents to dollars on index and show views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    @dollars = @product.price_in_dollars
   end
 
   def new
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,9 @@ class Product < ApplicationRecord
 
   validates :price, numericality: { only_integer: true }
 
+  def price_in_dollars
+    price_in_dollars = self.price.to_f / 100
+    sprintf("$%.2f", price_in_dollars)
+  end
+
 end

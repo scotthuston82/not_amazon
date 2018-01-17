9ff8bd0aa47d8f5c3eeb09a5bc25dc89ddcdf930
Danny Chang <dannychachang@gmail.com>
2018-01-17 14:01:22 -0500

    Add error messages for new product view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,7 +22,7 @@ class ProductsController < ApplicationController
     if @product.save
       redirect_to products_url
     else
-      redirect_to new_product_url
+      render :new
     end
   end
 

c6a82c8cf2ad5ed226a4af36e41db9331b21eab4
Danny Chang <dannychachang@gmail.com>
2018-01-17 12:06:17 -0500

    Add data to views for Products index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,22 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
 end
